3b8870e9f2200e37e6643ec09c98560e06cd5870
JuanLugoN <juanlugo@ciencias.unam.mx>
2021-08-01 16:31:49 -0500

    Implement get top products

--- app/products/views.py
+++ app/products/views.py
@@ -3,16 +3,23 @@ from flask_login import current_user, login_required
 
 from . import products
 from app.products import products_ctrl
+from app.user import user_ctrl
 
 @products.route('/home', methods = ['GET'])
 @login_required
 def home():
-    return render_template('home.html') 
+    role = user_ctrl.get_user_role()
+    if role == 'Comprador': 
+        products = products_ctrl.get_top_products()
+        return render_template('home.html', products = products, role = role)
+    return render_template('home.html', role = role) 
 
 
 @products.route('/upload_product', methods = ['GET','POST'])
 @login_required
 def upload_product():
+    if user_ctrl.get_user_role() != "Vendedor":
+        return render_template('404-error.html')
     if request.method == 'POST':
         name = request.form['name']
         price = request.form['price']
@@ -28,6 +35,8 @@ def upload_product():
 @products.route('/update_product', methods = ['GET','POST'])
 @login_required
 def update_product():
+    if user_ctrl.get_user_role() != "Vendedor":
+        return render_template('404-error.html')
     id_product = request.args.get('id')
     if not products_ctrl.verify_product(id_product):
         return render_template('404-error.html')
