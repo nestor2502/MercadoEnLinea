d17a3e2e5e58aab36c368d7c6c7803d1d8b7f2e4
José Fernando Brigido Pablo <ferbpp@ciencias.unam.mx>
2021-07-31 19:21:10 -0500

    fixed logout

--- app/user/views.py
+++ app/user/views.py
@@ -1,13 +1,12 @@
 from flask import render_template, redirect, url_for, request, jsonify
-from flask_login import current_user
+from flask_login import current_user, login_required
+from flask_login.utils import login_user, logout_user
 
 from . import user
 from app.user import user_ctrl
 
 @user.route('/signin', methods = ['POST', 'GET'])
 def signin():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
 
     if request.method == 'POST':
         email = request.form['email']
@@ -16,15 +15,15 @@ def signin():
         phone = request.form['phone']
 
         if user_ctrl.signin(email, name, phone, role):
-            return redirect(url_for('user.login'))
+            return redirect(url_for('index'))
         else:
             return redirect(url_for('user.signin'))
     return render_template('signin.html')
-    
+
 @user.route('/login', methods = ['POST', 'GET'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('products.home'))
 
     if request.method == 'POST':
         email = request.form['email']
@@ -35,3 +34,9 @@ def login():
         else:
             return redirect(url_for('user.login'))
     return render_template('login.html')
+
+@user.route('/logout')
+@login_required
+def logout():
+    user_ctrl.logout()
+    return redirect(url_for('user.login'))
